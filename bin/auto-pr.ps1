b40b5fdf380ac7e803f2eef895ffd526245e825f
reinhardclemenshadler <150024988+reinhardclemenshadler@users.noreply.github.com>
2024-01-18 11:04:15 +0100

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,8 +1,7 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "<reinhardclemenshadler>/<mybucketforhadler>:main"
 )
-
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
 $autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
 $dir = "$PSScriptRoot/../bucket" # checks the parent dir
