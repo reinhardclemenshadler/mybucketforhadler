45955f8cab5daf5629c06369c4dd0e5e33726069
reinhardclemenshadler <150024988+reinhardclemenshadler@users.noreply.github.com>
2024-01-18 12:08:19 +0100

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<reinhardclemenshadler>/<mybucketforhadler>:main"
+    [String]$upstream = "reinhardclemenshadler/mybucketforhadler:main"
 )
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
 $autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
